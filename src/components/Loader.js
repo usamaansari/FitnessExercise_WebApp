e124bf2a7b9e27ed4ace3fbd7182f1e588d537d7
Usama Ansari <usamaansari@Usamas-MacBook-Pro.local>
2022-08-13 14:09:46 -0700

    first commit

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+import { Stack } from '@mui/material';
+import { RevolvingDot } from 'react-loader-spinner';
+
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    <RevolvingDot color="grey" />
+  </Stack>
+);
+
+export default Loader;
